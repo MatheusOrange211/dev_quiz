4a8eba249d70670857f4d87a577f0bdd09968229
matheus <matheusnaranjocorrea@gmail.com>
2021-04-19 22:24:55 -0400

    first commit

---
+++ example/lib/splash/splash_page.dart
@@ -0,0 +1,19 @@
+// criand a splash da tela inicial de abertura do app
+import 'package:flutter/material.dart';
+import '../core/app_images.dart';
+import '../core/core.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppGradients.linear
+        ),
+        child: Center(
+          child:Image.asset(AppImages.logo) ,),
+      ),
+    );
+  }
+}
